4cd6c2d3d47cc57102dcae2062fdf9f3a6129104
Alex Gorischek <alex.gorischek@gmail.com>
2021-07-26 09:38:35 -0700

    Add more demo functionality

--- src/demo-only/demo.js
+++ src/demo-only/demo.js
@@ -1,4 +1,35 @@
-const expect = require(".");
+const { expect, test } = require(".");
 const Joi = require("joi");
 
-expect({ a: "hello" }).toMatchSchema({ a: Joi.string().required() });
+test("A number will not match a string schema", () => {
+  const schema = Joi.string();
+  const input = 3;
+  expect(input).toMatchSchema(schema);
+});
+
+test("A boolean property will not match a string property", () => {
+  const schema = Joi.object({
+    a: Joi.string(),
+  });
+  const input = {
+    a: false,
+  };
+  expect(input).toMatchSchema(schema);
+});
+
+test("A string will not match a string schema when negated", () => {
+  const schema = Joi.string();
+  const input = "a";
+  expect(input).not.toMatchSchema(schema);
+});
+
+test("A property will not match an empty object schema with options passed", () => {
+  const schema = Joi.object({});
+  const input = {
+    b: true,
+  };
+  const options = {
+    allowUnknown: false,
+  };
+  expect(input).toMatchSchema(schema, options);
+});
--- src/demo-only/index.js
+++ src/demo-only/index.js
@@ -12,21 +12,45 @@ function fail(message) {
   console.log(chalk.black.bgRed(failText) + "\n\n" + message);
 }
 
+function asyncUnsupported() {
+  console.log(
+    "This demo environment doesn't support async tests. Install jest-joi in your local environment instead!"
+  );
+}
+
 function makeMatcher(context, received) {
   return function toMatchSchema(schema) {
     const result = jestJoi.toMatchSchema.call(context, received, schema);
-    if (result.pass && context.not === false) pass();
-    else fail(result.message());
+    if (result.pass && context.isNot === false) pass();
+    else fail(result.message.call(context));
   };
 }
 
 function expect(received) {
   return {
-    toMatchSchema: makeMatcher({ not: false, promise: null }, received),
+    resolves: {
+      toMatchSchema: asyncUnsupported,
+      not: {
+        toMatchSchema: asyncUnsupported,
+      },
+    },
+    rejects: {
+      toMatchSchema: asyncUnsupported,
+      not: {
+        toMatchSchema: asyncUnsupported,
+      },
+    },
+    toMatchSchema: makeMatcher({ isNot: false, promise: "" }, received),
     not: {
-      toMatchSchema: makeMatcher({ not: true, promise: null }, received),
+      toMatchSchema: makeMatcher({ isNot: true, promise: "" }, received),
     },
   };
 }
 
-module.exports = expect;
+function test(name, fn) {
+  console.log(chalk.bold(name + "\n"));
+  fn();
+  console.log("\n");
+}
+
+module.exports = { expect, test };
