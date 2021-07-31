d50f7f5164a8ab1bf9d1406ab0d68aebecaffcb7
Alex Gorischek <alex.gorischek@gmail.com>
2021-07-31 17:58:21 +0000

    Update type definitions

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export * from "./demo";
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toMatchSchema(schema: Joi.Schema, options?: Joi.ValidationOptions): R;
+      toMatchSchema(schema: unknown, options?: Joi.ValidationOptions): R;
       toBeSchema(): R;
       toBeSchemaLike(): R;
     }
