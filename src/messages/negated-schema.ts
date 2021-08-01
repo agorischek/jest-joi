11fb71b19c36662cbbec7ed48b95ca1901d336fb
Alex Gorischek <alex.gorischek@gmail.com>
2021-08-01 03:28:07 +0000

    Add newline to error message

--- src/messages/negated-schema.ts
+++ src/messages/negated-schema.ts
@@ -2,5 +2,5 @@ import { Hint, Schema } from "../classes";
 import { labels, validSchema } from "./shared";
 
 export function negatedSchemaMessage(hint: Hint, schema: Schema): string[] {
-  return [hint.text, "", labels.schema + " " + validSchema(schema)];
+  return [hint.text, "", labels.schema, validSchema(schema)];
 }
